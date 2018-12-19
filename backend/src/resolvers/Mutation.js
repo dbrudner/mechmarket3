7e10ccc5035ff0d3cab5f2a91ab100c7ed75bad0
Dave Brudner <dbrudne1@gmail.com>
2018-12-18 19:54:09 -0600

    reset password

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { randomBytes } = require("crypto");
+const { promisify } = require("util");
 
 const Mutations = {
 	async createKeyboard(parent, args, ctx, info) {
@@ -97,6 +99,69 @@ const Mutations = {
 	async signout(parent, args, ctx, info) {
 		ctx.response.clearCookie("token");
 		return { message: "Signed out" };
+	},
+	async requestReset(parent, args, ctx, info) {
+		const user = await ctx.db.query.user({ where: { email: args.email } });
+		if (!user) {
+			throw new Error(`No user found with email ${args.email}`);
+		}
+
+		const resetToken = (await promisify(randomBytes)(20)).toString("hex");
+		const resetTokenExpiry = Date.now() + 36000000;
+
+		const res = await ctx.db.mutation.updateUser({
+			where: { email: args.email },
+			data: {
+				resetToken,
+				resetTokenExpiry
+			}
+		});
+
+		console.log(res);
+		// '90ebd7ee86f05e91f874ebc614225bb1e840eb7d'
+
+		return { message: "getting new password" };
+	},
+	async resetPassword(parent, args, ctx, info) {
+		const { resetToken, newPassword, confirmPassword } = args;
+
+		if (newPassword !== confirmPassword) {
+			throw new Error("Passwords don't match");
+		}
+
+		const [user] = await ctx.db.query.users({
+			where: {
+				resetToken,
+				resetTokenExpiry_gte: Date.now() - 36000000
+			}
+		});
+
+		if (!user) {
+			throw new Error("Token either expired or invalid");
+		}
+
+		const password = await bcrypt.hash(args.newPassword, 10);
+
+		const updatedUser = await ctx.db.mutation.updateUser({
+			where: {
+				email: user.email
+			},
+			data: {
+				password,
+				resetToken: null,
+				resetTokenExpiry: null
+			}
+		});
+
+		const token = jwt.sign(
+			{ userId: updatedUser.id },
+			process.env.APP_SECRET
+		);
+		ctx.response.cookie("token", token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365
+		});
+		return updatedUser;
 	}
 };
 
